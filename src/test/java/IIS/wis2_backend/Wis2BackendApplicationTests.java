55c85b7f7858277dc100595691e804bedce203ec
Igor Lacko <iginonajlepsi@gmail.com>
2025-09-24 11:26:25 +0200

    Initial commit

---
+++ src/test/java/IIS/wis2_backend/Wis2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package IIS.wis2_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wis2BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
