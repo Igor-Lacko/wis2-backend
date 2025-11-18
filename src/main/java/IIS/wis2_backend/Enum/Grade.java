ac96461108af3c752a254b755c8f010cbfd67aa1
Igor Lacko <iginonajlepsi@gmail.com>
2025-11-18 11:57:13 +0100

    feat: studentcourse model

---
+++ src/main/java/IIS/wis2_backend/Enum/Grade.java
@@ -0,0 +1,13 @@
+package IIS.wis2_backend.Enum;
+
+/**
+ * Enum of grades.
+ */
+public enum Grade {
+    A,
+    B,
+    C,
+    D,
+    E,
+    F
+}
---
+++ src/main/java/IIS/wis2_backend/Models/Relational/StudentCourse.java
@@ -0,0 +1,67 @@
+package IIS.wis2_backend.Models.Relational;
+
+import IIS.wis2_backend.Models.Course;
+import IIS.wis2_backend.Models.User.Student;
+import jakarta.persistence.*;
+import lombok.*;
+
+/**
+ * Model representing a relation student --> studentcourse <-- course
+ */
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class StudentCourse {
+    /**
+     * Relation ID.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * The student in the relation.
+     */
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    private Student student;
+
+    /**
+     * The course in the relation.
+     */
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    private Course course;
+
+    /**
+     * Number of points the student has earned in this course.
+     */
+    @Column(nullable = false)
+    private Integer points;
+
+    /**
+     * If the student has earned the unit credit for this course. Can be nullable (exam-only courses).
+     */
+    private Boolean unitCredit;
+
+    /**
+     * If the student has passed the exam for this course. Also can be nullable.
+     */
+    private Boolean examPassed;
+    
+    /**
+     * Final grade for the course. Can also be nullable (non-classified unit-credit courses).
+     */
+    private Double finalGrade;
+
+    /**
+     * If the student has completed the course.
+     */
+    private Boolean completed;
+
+    /**
+     * If the course is over. This means if !completed at the time this is true, the student has failed the course.
+     */
+    @Column(nullable = false)
+    private Boolean courseOver;
+}
