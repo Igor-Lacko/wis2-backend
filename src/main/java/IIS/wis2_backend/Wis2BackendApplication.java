2b598e430a7d043b0138bd5b7eeaa3b4888fa3e7
jakub kapitulcin <kapitulcinjakub1@gmail.com>
2025-11-20 15:37:12 +0100

    add admin into the mix

--- src/main/java/IIS/wis2_backend/Wis2BackendApplication.java
+++ src/main/java/IIS/wis2_backend/Wis2BackendApplication.java
@@ -1,13 +1,39 @@
 package IIS.wis2_backend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import IIS.wis2_backend.Enum.Roles;
+import IIS.wis2_backend.Models.User.Wis2User;
+import IIS.wis2_backend.Repositories.User.UserRepository;
 
 @SpringBootApplication
 public class Wis2BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Wis2BackendApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner insertAdmin(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+		return args -> {
+			if (!userRepository.existsByUsername("admin")) {
+				Wis2User admin = Wis2User.builder()
+						.username("admin")
+						.firstName("Admin")
+						.lastName("Admin")
+						.email("admin@wis2.com")
+						.password(passwordEncoder.encode("admin"))
+						.birthday(java.sql.Date.valueOf("2000-01-01"))
+						.role(Roles.ADMIN)
+						.activated(true)
+						.build();
+				userRepository.save(admin);
+			}
+		};
 	}
 
 }
